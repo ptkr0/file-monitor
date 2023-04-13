bd8f86b2a8102355062ccff48d8fbf14bcb110a4
ptkr0 <piotrradziszewski@outlook.com>
2023-04-13 02:01:37 +0200

    demon teraz wywali blad do sysloga bez argumentow

--- test.c
+++ test.c
@@ -12,21 +12,21 @@ void validate_paths(char *path)
     /* if stat() returns a non-zero value an error occured */
     if (stat(path, &STATS) != 0)
     {
-        printf("zla sciezka\n");
+        printf("podales zla nazwe katalogu %s\n", path);
         exit(EXIT_FAILURE);
     }
 
     /* S_ISDIR returns 1 if the file type is a directory */
     if (S_ISDIR(STATS.st_mode))
-        printf("dziala\n");
+        printf("dziala %s\n", path);
     else
-        printf("nie dziala\n");
+        printf("nie dziala %s\n", path);
 }
 
 int main(int argc, char* argv[])
 {
     /* if there was no arguments provided */
-    if ((argc < 2) && (argc > 3)) 
+    if (argc != 3) 
     {
         exit(EXIT_FAILURE);
     }
